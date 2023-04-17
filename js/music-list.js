bc5e54e413eb34532c84fdd47918b44d10d2a084
RzkyyDev <74136457+Rizky878@users.noreply.github.com>
2023-04-17 20:45:41 +0700

    Add files via upload

--- js/music-list.js
+++ js/music-list.js
@@ -1,1 +1,63 @@
 
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+  name: "Perfect Speed Up",
+    artist: "Speed Up",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+  name: "Bernafas Tanpamu Speed Up",
+    artist: "Speed Up",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+  name: "Cars Outside Speed Up",
+    artist: "Speed Up",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+  name: "Hapus Aku Speed Up",
+    artist: "Speed Up",
+    img: "music-10",
+    src: "music-10"
+  }
+];
